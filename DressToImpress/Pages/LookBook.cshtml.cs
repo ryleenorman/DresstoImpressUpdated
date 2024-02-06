f3c491e696fc1a88d3e29685fd6970e3de8a7018
ryleenorman <rln00005@mix.wvu.edu>
2024-02-06 15:01:16 -0500

    Final Update

---
+++ DressToImpress/Pages/LookBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DresstoImpress.Pages
+{
+    public class LookBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DressToImpress/Pages/OutfitResults.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DresstoImpress.Pages
+{
+    public class OutfitResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
